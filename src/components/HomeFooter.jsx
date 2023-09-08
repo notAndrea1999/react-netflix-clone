250241808c23699ee73e71e13f185ba494a27304
Andrea De Masi <andreademasi1999@gmail.com>
2023-09-08 16:12:31 +0200

    fixing hamburger menu color

--- src/components/HomeFooter.jsx
+++ src/components/HomeFooter.jsx
@@ -12,19 +12,19 @@ class HomeFooter extends Component {
             <i class="bi bi-instagram me-5 fs-3"></i>
             <i class="bi bi-youtube me-5 fs-3"></i>
           </Col>
-          <Col className="col-3 text-secondary">Audio and Subtitles</Col>
-          <Col className="col-3 text-secondary">Media Center</Col>
-          <Col className="col-3 text-secondary">Privacy</Col>
-          <Col className="col-3 text-secondary">Contact us</Col>
-          <Col className="col-3 text-secondary">Audio Description</Col>
-          <Col className="col-3 text-secondary">Investor Relations</Col>
-          <Col className="col-3 text-secondary">Legal Notices</Col>
-          <Col className="col-3 text-secondary">Help Center</Col>
-          <Col className="col-3 text-secondary">Jobs</Col>
-          <Col className="col-3 text-secondary">Cookie Preferences</Col>
-          <Col className="col-3 text-secondary">Gift Cards</Col>
-          <Col className="col-3 text-secondary">Terms of Use</Col>
-          <Col className="col-3 text-secondary">Test</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Audio and Subtitles</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Media Center</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Privacy</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Contact us</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Audio Description</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Investor Relations</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Legal Notices</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Help Center</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Jobs</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Cookie Preferences</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Gift Cards</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Terms of Use</Col>
+          <Col className="col-6 col-md-6 col-lg-4 text-secondary">Test</Col>
           <Col className="col-12">
             <Button variant="secondary-outline" className="text-secondary border-secondary mt-1">
               Service Code
